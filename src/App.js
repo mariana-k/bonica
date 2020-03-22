656b1c8ddeb89bb1b0c1ab0e0d3913dacd8559cd
mariana-k <mariana.kyrkosh@gmail.com>
2020-03-22 20:32:23 +0100

    added email sign up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -21,9 +21,26 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      console.log(user)
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data,
+            }
+          }, () => {
+            console.log(this.state)
+          });
+        });
+      } else {
+        this.setState({
+          currentUser: userAuth,
+        })
+      }
+     
     });
   }
   componentWillUnmount() {
